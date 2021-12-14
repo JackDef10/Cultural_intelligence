d02395ebc894d800e9865b4f4e64cb480122cde4
Jack D <Jackdefroand@gmail.com>
2021-12-14 16:06:48 +0000

    added a google translator api

---
+++ assets/js/google-translate.js
@@ -0,0 +1,6 @@
+function translateBtn() {
+    document.getElementById("google_translate_element").style.visibility = "visible"
+    document.getElementById("translate-image").style.visibility = "hidden"
+    document.getElementById("google-translate-btn").style.visibility = "hidden"
+  }
+
